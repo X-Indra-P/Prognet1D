facc2c3efea22ef5487814471d87c37772cb3895
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-12 19:59:52 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -1,63 +1,32 @@
-<?php
-if (isset($_POST['submit'])) {
-    $nama_lengkap = $_POST['nama_lengkap'];
-    $NIM = $_POST['NIM'];
-    $alamat = $_POST['Alamat'];
-    $jeniskelamin = $_POST['Jenis Kelamin'];
-    $jurusan = $_POST['Jurusan'];
-    $email = $_POST['Email'];
-
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa Teknik</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Form Submission Result</title>
 </head>
-
 <body>
-    <div class="wrap">
-        <div class="container">
-            <h2>Data Hasil Pendaftaran</h2>
-            <table>
-                <tr>
-                    <td>Nama Lengkap</td>
-                    <td>:</td>
-                    <td><?= $nama_lengkap  ?></td>
-                </tr>
-                <tr>
-                    <td>NIM</td>
-                    <td>:</td>
-                    <td><?= $NIM  ?></td>
-                </tr>
-                <tr>
-                    <td>Alamat</td>
-                    <td>:</td>
-                    <td><?= $alamat  ?></td>
-                </tr>
-                <tr>  
-                    <td>Jenis Kelamin</td>
-                    <td>:</td>
-                    <td><?= $jeniskelamin  ?></td>
-                </tr>
-                <tr>
-                    <td>Jurusan</td>
-                    <td>:</td>
-                    <td><?= $jurusan  ?></td>
-                </tr>
-                <tr>    
-                    <td>Email</td>
-                    <td>:</td>
-                    <td><?= $email  ?></td>
-                </tr>
-            </table>
-            </fieldset>
-        </div>
-</body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve data from the form
+        $nama = $_POST["nama"];
+        $nim = $_POST["nim"];
+        $alamat = $_POST["alamat"];
+        $jenis_kelamin = $_POST["jenis_kelamin"];
+        $jurusan = $_POST["jurusan"];
+        $email = $_POST["email"];
 
+        // Display the submitted data
+        echo "<h1>Form Submission Result</h1>";
+        echo "<p>Nama Lengkap: $nama</p>";
+        echo "<p>NIM: $nim</p>";
+        echo "<p>Alamat: $alamat</p>";
+        echo "<p>Jenis Kelamin: $jenis_kelamin</p>";
+        echo "<p>Jurusan: $jurusan</p>";
+        echo "<p>Email: $email</p>";
+    } else {
+        echo "<p>Form submission error.</p>";
+    }
+    ?>
+</body>
 </html>
