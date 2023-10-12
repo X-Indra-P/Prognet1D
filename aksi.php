8372cd300bf9644caf16e58f98e34e3bf4ea47cd
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-12 20:14:41 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -1,39 +1,32 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama = $_POST["nama"];
-    $nim = $_POST["nim"];
-    $alamat = $_POST["alamat"];
-    $jenis_kelamin = $_POST["jenis_kelamin"];
-    $jurusan = $_POST["jurusan"];
-    $email = $_POST["email"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasil Formulir Biodata</title>
+</head>
+<body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve data from the form
+        $nama = $_POST["nama"];
+        $nim = $_POST["nim"];
+        $alamat = $_POST["alamat"];
+        $jenis_kelamin = $_POST["jenis_kelamin"];
+        $jurusan = $_POST["jurusan"];
+        $email = $_POST["email"];
 
-    $formData = [
-        "Nama Lengkap: $nama",
-        "NIM: $nim",
-        "Alamat: $alamat",
-        "Jenis Kelamin: $jenis_kelamin",
-        "Jurusan: $jurusan",
-        "Email: $email"
-    ];
-
-    if (!empty($jurusan)) {
-        $formData[] = "Jurusan Yang Anda Pilih:";
-        foreach ($jurusan as $s) {
-            $formData[] = "- $s";
-        }
+        // Display the submitted data
+        echo "<h1>Form Submission Result</h1>";
+        echo "<p>Nama Lengkap: $nama</p>";
+        echo "<p>NIM: $nim</p>";
+        echo "<p>Alamat: $alamat</p>";
+        echo "<p>Jenis Kelamin: $jenis_kelamin</p>";
+        echo "<p>Jurusan: $jurusan</p>";
+        echo "<p>Email: $email</p>";
     } else {
-        $formData[] = "Anda belum memilih jurusan.";
+        echo "<p>Form submission error.</p>";
     }
-
-    $formDataString = implode("\n", $formData);
-    $fileName = "HasilData.txt";
-    $formDataString .= "\n";
-
-    file_put_contents($fileName, $formDataString, FILE_APPEND);
-
-    echo "<h1>Form Submission Result</h1>";
-    echo "<p>Data telah disimpan ke <strong>$fileName</strong>.</p>";
-} else {
-    echo "<p>Terjadi kesalahan dalam pengiriman formulir.</p>";
-}
-?>
+    ?>
+</body>
+</html>
