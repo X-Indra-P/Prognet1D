e84cde06c4c08665ad1555ebb4af7ea0b4fb1ab2
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-17 20:04:42 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -1,29 +1,25 @@
 <?php
-// Koneksi ke database (gantilah dengan informasi koneksi Anda)
-$koneksi = mysqli_connect("localhost", "root", "prognet1D");
+// Mengambil koneksi ke database (pastikan Anda telah membuat koneksi seperti yang telah dijelaskan sebelumnya)
+include('koneksi.php');
 
-if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Mengambil data dari formulir
+    $nama = $_POST['nama'];
+    $nim = $_POST['nim'];
+    $alamat = $_POST['alamat'];
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $jurusan = $_POST['jurusan'];
+    $email = $_POST['email'];
+
+    // Menyimpan data ke database (operasi Create)
+    $sql = "INSERT INTO biodata (nama, nim, alamat, jenis_kelamin, jurusan, email) 
+            VALUES ('$nama', '$nim', '$alamat', '$jenis_kelamin', '$jurusan', '$email')";
+    if (mysqli_query($koneksi, $sql)) {
+        echo "Data berhasil disimpan ke database.";
+    } else {
+        echo "Gagal menyimpan data: " . mysqli_error($koneksi);
+    }
 }
-
-// Ambil data dari formulir
-$nama = $_POST['nama'];
-$nim = $_POST['nim'];
-$alamat = $_POST['alamat'];
-$jenis_kelamin = $_POST['jenis_kelamin'];
-$jurusan = $_POST['jurusan'];
-$email = $_POST['email'];
-
-// Query untuk menyimpan data ke database
-$sql = "INSERT INTO biodata (nama, nim, alamat, jenis_kelamin, jurusan, email) 
-        VALUES ('$nama', '$nim', '$alamat', '$jenis_kelamin', '$jurusan', '$email')";
-
-if (mysqli_query($koneksi, $sql)) {
-    echo "Data berhasil disimpan";
-} else {
-    echo "Gagal menyimpan data: " . mysqli_error($koneksi);
-}
-
-// Tutup koneksi database
+// Tutup koneksi database (pastikan Anda telah membuatnya di "koneksi.php")
 mysqli_close($koneksi);
 ?>
