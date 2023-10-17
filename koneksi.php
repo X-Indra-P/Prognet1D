3eb80fdc308bacb915d1638158626b4566a0016e
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-17 20:24:44 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
-$username = "root";
-$database = "prognetd1";
+$host = "localhost"; // Ganti dengan host database Anda
+$username = "root"; // Ganti dengan nama pengguna database Anda
+$database = "prognetd1"; // Ganti dengan nama database Anda
 
-$koneksi = mysqli_connect($host, $username, $password, $database);
+$koneksi = mysqli_connect($host, $username, $database);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
