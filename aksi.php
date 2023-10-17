1c4b5a420dafa32c3b6fbe10df07e9803aff3d6f
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-17 20:54:37 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -1,23 +1,23 @@
 <?php
-include('koneksi.php');
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nama = $_POST['nama'];
+	$nama = $_POST['nama'];
     $nim = $_POST['nim'];
     $alamat = $_POST['alamat'];
     $jenis_kelamin = $_POST['jenis_kelamin'];
     $jurusan = $_POST['jurusan'];
     $email = $_POST['email'];
 
-    $sql = "INSERT INTO biodata (nama, nim, alamat, jenis_kelamin, jurusan, email) 
-            VALUES ('$nama', '$nim', '$alamat', '$jenis_kelamin', '$jurusan', '$email')";
-
-    if (mysqli_query($koneksi, $sql)) {
-        echo "Data berhasil disimpan ke database.";
-    } else {
-        echo "Gagal menyimpan data: " . mysqli_error($koneksi);
-    }
-}
-
-mysqli_close($koneksi);
+	// Database connection
+	$conn = new mysqli('localhost','root','','prognetd1');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(nama, nim, alamat, jenis_kelamin, jurusan, email) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $nama, $nim, $alamat, $jenis_kelamin, $jurusan, $email);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Data Berhasil Disimpan Ke Database!";
+		$stmt->close();
+		$conn->close();
+	}
 ?>
