40a6f3bcf5fdf0af66c8aecaaf546b6f33e3d9fa
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-17 20:18:17 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,18 +1,11 @@
 <?php
-include('koneksi.php'); // Mengambil koneksi ke database
-$sql = "SELECT * FROM form_biodata";
-$result = mysqli_query($koneksi, $sql);
+$host = "localhost";
+$username = "root";
+$database = "prognetd1";
 
-if (mysqli_num_rows($result) > 0) {
-    echo "<table>";
-    echo "<tr><th>Nama</th><th>NIM</th><th>Alamat</th><th>Jenis Kelamin</th><th>Jurusan</th><th>Email</th></tr>";
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>" . $row["nama"] . "</td><td>" . $row["nim"] . "</td><td>" . $row["alamat"] . "</td><td>" . $row["jenis_kelamin"] . "</td><td>" . $row["jurusan"] . "</td><td>" . $row["email"] . "</td></tr>";
-    }
-    echo "</table>";
-} else {
-    echo "Tidak ada data yang ditemukan";
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-mysqli_close($koneksi); // Tutup koneksi database
 ?>
