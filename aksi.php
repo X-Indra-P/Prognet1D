8df60e0f6755a9baae9750f395a0855f582bb0c0
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-17 20:25:41 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -1,9 +1,7 @@
 <?php
-// Mengambil koneksi ke database (pastikan Anda telah membuat koneksi seperti yang telah dijelaskan sebelumnya)
 include('koneksi.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Mengambil data dari formulir
     $nama = $_POST['nama'];
     $nim = $_POST['nim'];
     $alamat = $_POST['alamat'];
@@ -11,15 +9,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $jurusan = $_POST['jurusan'];
     $email = $_POST['email'];
 
-    // Menyimpan data ke database (operasi Create)
     $sql = "INSERT INTO biodata (nama, nim, alamat, jenis_kelamin, jurusan, email) 
             VALUES ('$nama', '$nim', '$alamat', '$jenis_kelamin', '$jurusan', '$email')";
+
     if (mysqli_query($koneksi, $sql)) {
         echo "Data berhasil disimpan ke database.";
     } else {
         echo "Gagal menyimpan data: " . mysqli_error($koneksi);
     }
 }
-// Tutup koneksi database (pastikan Anda telah membuatnya di "koneksi.php")
+
 mysqli_close($koneksi);
 ?>
