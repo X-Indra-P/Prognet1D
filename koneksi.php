91825dadfe9a75e9bac9c43f6696a2c2880ef602
Candra7890 <115153167+Candra7890@users.noreply.github.com>
2023-10-19 16:38:25 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,9 +2,9 @@
 	$host = 'localhost:3307';
 	$user = 'root';
 	$pass = '';
-	$db = 'tb_siswa';
+	$db = 'prognetd1';
 
-	$conn = mysqli($host, $user, $pass, $db);
+	$conn = mysqli_connect($host, $user, $pass, $db);
 	
 	if($conn){
 		//echo "Koneksi Berhasil";
